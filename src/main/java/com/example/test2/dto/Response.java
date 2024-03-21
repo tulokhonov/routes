2ef95fde9493d52a6d705927af9e3bcff3f066f7
Тулохонов Эдуард Александрович <e.tulokhonov@bankffin.ru>
2024-03-21 23:52:06 +0300

    cleanup

--- src/main/java/com/example/test2/dto/Response.java
+++ src/main/java/com/example/test2/dto/Response.java
@@ -1,28 +1,4 @@
 package com.example.test2.dto;
 
-public class Response {
-
-    private final Integer from;
-
-    private final Integer to;
-
-    private final boolean direct;
-
-    public Response(Integer from, Integer to, boolean direct) {
-        this.from = from;
-        this.to = to;
-        this.direct = direct;
-    }
-
-    public Integer getFrom() {
-        return from;
-    }
-
-    public Integer getTo() {
-        return to;
-    }
-
-    public boolean isDirect() {
-        return direct;
-    }
+public record Response(Integer from, Integer to, boolean direct) {
 }
